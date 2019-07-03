a9ad1ae757e092c7910496b325859ea7fb102379
Olivier Leger <olivierleger@gmail.com>
2019-07-03 13:15:23 -0400

    Use old database name variable for retro-compatibility

--- helpers/config.py
+++ helpers/config.py
@@ -823,9 +823,10 @@ class Config:
         Settings can be tweaked thanks to pgconfig.org API
         """
         self.__config["kc_postgres_db"] = CLI.colored_input("KoBoCat PostgreSQL database", CLI.COLOR_SUCCESS,
-                                                         self.__config.get("kc_postgres_db"))
+                                                            self.__config.get("postgres_db",
+                                                                              self.__config.get("kc_postgres_db")))
         self.__config["kpi_postgres_db"] = CLI.colored_input("KPI PostgreSQL database", CLI.COLOR_SUCCESS,
-                                                            self.__config.get("kpi_postgres_db"))
+                                                             self.__config.get("kpi_postgres_db"))
         self.__config["postgres_user"] = CLI.colored_input("Postgres user", CLI.COLOR_SUCCESS,
                                                            self.__config.get("postgres_user"))
         self.__config["postgres_password"] = CLI.colored_input("Postgres password", CLI.COLOR_SUCCESS,
