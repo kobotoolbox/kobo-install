da06b6a236026878d67537cd6b8aae0259bcec71
John N. Milner <john@tmoj.net>
2020-06-17 13:18:14 -0400

    Use release 2.020.25 of kobo-docker

--- helpers/config.py
+++ helpers/config.py
@@ -28,7 +28,7 @@ class Config:
     DEFAULT_PROXY_PORT = "8080"
     DEFAULT_NGINX_PORT = "80"
     DEFAULT_NGINX_HTTPS_PORT = "443"
-    KOBO_DOCKER_BRANCH = '2.020.24b'
+    KOBO_DOCKER_BRANCH = '2.020.25'
     KOBO_INSTALL_BRANCH = 'master'  # be mindful of how `--update` works
     KOBO_INSTALL_VERSION = '2.4.2'
 
