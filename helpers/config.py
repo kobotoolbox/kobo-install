7219d29abbdf6a6615f4578759e25ec71e494610
Joshua Beretta <joshuaberetta@gmail.com>
2020-10-07 15:10:37 +0200

    changing KOBO_DOCKER_BRANCH in helpers/config.py

--- helpers/config.py
+++ helpers/config.py
@@ -32,7 +32,7 @@ class Config(with_metaclass(Singleton)):
     DEFAULT_PROXY_PORT = "8080"
     DEFAULT_NGINX_PORT = "80"
     DEFAULT_NGINX_HTTPS_PORT = "443"
-    KOBO_DOCKER_BRANCH = '300-backups-venv-failure'
+    KOBO_DOCKER_BRANCH = 'support-kobokitten'
     KOBO_INSTALL_VERSION = '3.2.0'
 
     def __init__(self):
