338e35ce8d80a7c8a07ad2c961e0a57e3cb14c9a
John N. Milner <john@tmoj.net>
2021-03-16 18:14:49 -0400

    Use `beta` branch of kobo-docker

--- helpers/config.py
+++ helpers/config.py
@@ -30,7 +30,7 @@ class Config(metaclass=Singleton):
     DEFAULT_PROXY_PORT = '8080'
     DEFAULT_NGINX_PORT = '80'
     DEFAULT_NGINX_HTTPS_PORT = '443'
-    KOBO_DOCKER_BRANCH = 'support-kobocat-2.0'
+    KOBO_DOCKER_BRANCH = 'beta'
     KOBO_INSTALL_VERSION = '5.0.0-dev'
     MAXIMUM_AWS_CREDENTIAL_ATTEMPTS = 3
 
