768bc0f171e7b29d2d7d360570ad241130500a15
Olivier Leger <olivierleger@gmail.com>
2021-08-27 06:00:26 -0400

    Bump version to pre6.0.0

--- helpers/config.py
+++ helpers/config.py
@@ -31,7 +31,7 @@ class Config(metaclass=Singleton):
     DEFAULT_NGINX_PORT = '80'
     DEFAULT_NGINX_HTTPS_PORT = '443'
     KOBO_DOCKER_BRANCH = '2.021.30'
-    KOBO_INSTALL_VERSION = '5.0.0'
+    KOBO_INSTALL_VERSION = 'pre-6.0.0'
     MAXIMUM_AWS_CREDENTIAL_ATTEMPTS = 3
 
     def __init__(self):
