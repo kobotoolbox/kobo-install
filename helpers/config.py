b77dace724f28224eb7eadee8f0651176277dfc9
Olivier Leger <olivierleger@gmail.com>
2023-02-13 17:16:21 -0500

    Bump version to 8.0.1

--- helpers/config.py
+++ helpers/config.py
@@ -32,7 +32,7 @@ class Config(metaclass=Singleton):
     DEFAULT_NGINX_PORT = '80'
     DEFAULT_NGINX_HTTPS_PORT = '443'
     KOBO_DOCKER_BRANCH = '2.023.04b'
-    KOBO_INSTALL_VERSION = '8.0.0'
+    KOBO_INSTALL_VERSION = '8.0.1'
     MAXIMUM_AWS_CREDENTIAL_ATTEMPTS = 3
     ALLOWED_PASSWORD_CHARACTERS = (
         string.ascii_letters
