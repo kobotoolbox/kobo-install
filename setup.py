b23300d35f3d87753d8a6b91c8fef15566949c81
Karine ZHU <karine.zhu@solution-bi.com>
2023-02-27 15:02:09 +0700

    test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version=Config.KOBO_INSTALL_VERSION,
     # Include all the python modules except `tests`,
     packages=find_packages(exclude=['tests']),
-    url='https://github.com/kobotoolbox/kobo-install/',
+    url='https://github.com/KarineSBI/kobo-install/',
     license='',
     author='KoboToolbox',
     author_email='',
